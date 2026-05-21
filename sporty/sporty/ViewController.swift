c9d08d218a0300c0fd0fc6c9239ac0ff895ef9b9
Ahmad <ahmad@Ahmads-Mac.local>
2026-05-21 02:58:23 -0700

    MVP Architecture

---
+++ sporty/sporty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sporty
+//
+//  Created by Ahmad on 21/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
