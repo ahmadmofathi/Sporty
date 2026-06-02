4f521fb7a57c4e91f70d44b3cb59ff7505aa92ce
Shady <shadyramadan.sr@gmail.com>
2026-06-02 16:59:37 +0300

    Saving and fetching and deleting data from coreData and implement core data manger saving data from leauge page amd fetch it to favorite page

--- sporty/sporty/Module/LeaguePage/Presenter/LeaguePresenter.swift
+++ sporty/sporty/Module/LeaguePage/Presenter/LeaguePresenter.swift
@@ -7,37 +7,54 @@
 
 import Foundation
 
+protocol LeaguesViewProtocol: AnyObject {
+    func displayLeagues(_ names: [String], countries: [String], logos: [String], favorites: [Bool])
+    func navigateToLeague(with leagueName: String)
+}
+
 class LeaguePresenter {
     private weak var view: LeaguesViewProtocol?
-   
     private var leagues: [League] = []
-    var Sport : String?
+    var Sport: String?
     
-    init(view: LeaguesViewProtocol, sport : String) {
+    init(view: LeaguesViewProtocol, sport: String) {
         self.view = view
         self.Sport = sport
     }
     
     func viewDidLoad() {
-       
-        NetworkManager.shared.fetchLeagues(sport: self.Sport! ) { [weak self] result in
+        NetworkManager.shared.fetchLeagues(sport: self.Sport!) { [weak self] result in
             guard let self = self else { return }
-            
             switch result {
             case .success(let fetchedLeagues):
                 self.leagues = fetchedLeagues
-                
-    
-                let names = fetchedLeagues.map { $0.leagueName ?? "Unknown League" }
-                let countries = fetchedLeagues.map { $0.countryName ?? "Unknown Country" }
-                let logos = fetchedLeagues.map { $0.leagueLogo ?? "" }
-                
-                self.view?.displayLeagues(names, countries: countries, logos: logos)
-                
+                self.updateView()
             case .failure(let error):
                 print("Error loading leagues: \(error.localizedDescription)")
-                    }
+            }
         }
+    }
+    
+    func toggleFavorite(at index: Int) {
+        guard index < leagues.count else { return }
+        let league = leagues[index]
+        guard let key = league.leagueKey else { return }
+        
+        if CoreDataManager.shared.isLeagueFavorite(byKey: key) {
+            CoreDataManager.shared.deleteLeague(byKey: key)
+        } else {
+            CoreDataManager.shared.saveLeague(league, sport: self.Sport ?? "football")
+        }
+        updateView()
+    }
+    
+    private func updateView() {
+        let names = leagues.map { $0.leagueName ?? "Unknown League" }
+        let countries = leagues.map { $0.countryName ?? "Unknown Country" }
+        let logos = leagues.map { $0.leagueLogo ?? "" }
+        let favorites = leagues.map { CoreDataManager.shared.isLeagueFavorite(byKey: $0.leagueKey ?? 0) }
+        
+        self.view?.displayLeagues(names, countries: countries, logos: logos, favorites: favorites)
     }
     
     func didSelectLeague(at index: Int) {
--- sporty/sporty/Module/LeaguePage/View/LeagueViewController.swift
+++ sporty/sporty/Module/LeaguePage/View/LeagueViewController.swift
@@ -1,16 +1,5 @@
-//
-//  LeaguesViewController.swift
-//  sporty
-//
-//  Created by Shady Ramadan on 25/05/2026.
-//
-
 import UIKit
 import SDWebImage
-protocol LeaguesViewProtocol: AnyObject {
-    func displayLeagues(_ names: [String], countries: [String], logos: [String])
-    func navigateToLeague(with leagueName: String)
-}
 
 class LeaguesViewController: UIViewController, LeaguesViewProtocol {
 
@@ -20,7 +9,9 @@ class LeaguesViewController: UIViewController, LeaguesViewProtocol {
     private var leagueNames: [String] = []
     private var leagueCountries: [String] = []
     private var leagueLogos: [String] = []
-    var selectedSport : String?
+    private var leagueFavorites: [Bool] = []
+    var selectedSport: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Leagues"
@@ -28,16 +19,16 @@ class LeaguesViewController: UIViewController, LeaguesViewProtocol {
         tableView.delegate = self
         tableView.dataSource = self
         let sportToFetch = selectedSport ?? "football"
-        presenter = LeaguePresenter(view: self, sport : sportToFetch)
+        presenter = LeaguePresenter(view: self, sport: sportToFetch)
         presenter.viewDidLoad()
     }
     
-    func displayLeagues(_ names: [String], countries: [String], logos: [String]) {
+    func displayLeagues(_ names: [String], countries: [String], logos: [String], favorites: [Bool]) {
         self.leagueNames = names
         self.leagueCountries = countries
         self.leagueLogos = logos
+        self.leagueFavorites = favorites
         
-        // التحديث على الـ Main Thread لأن الـ API شغال في الخلفية
         DispatchQueue.main.async {
             self.tableView.reloadData()
         }
@@ -45,7 +36,7 @@ class LeaguesViewController: UIViewController, LeaguesViewProtocol {
     
     func navigateToLeague(with leagueName: String) {
         let ml = UIStoryboard(name: "MainLeague", bundle: nil)
-        if let MainLeagueVC = ml.instantiateViewController(withIdentifier: "MainLeagueVC") as? MainLeagueViewController{
+        if let MainLeagueVC = ml.instantiateViewController(withIdentifier: "MainLeagueVC") as? MainLeagueViewController {
             self.navigationController?.pushViewController(MainLeagueVC, animated: true)
         }
     }
@@ -62,28 +53,31 @@ extension LeaguesViewController: UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         
-        let name = leagueNames[indexPath.row]
-        let country = leagueCountries[indexPath.row]
-        let logoURLString = leagueLogos[indexPath.row]
-        
-        cell.leagueNameLabel.text = name
-        cell.countryLabel.text = country
+        cell.leagueNameLabel.text = leagueNames[indexPath.row]
+        cell.countryLabel.text = leagueCountries[indexPath.row]
+        let isFav = leagueFavorites[indexPath.row]
+        let heartImage = isFav ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
+        cell.favoriteButton.setImage(heartImage, for: .normal)
+                cell.onFavoriteTapped = { [weak self] in
+            self?.presenter.toggleFavorite(at: indexPath.row)
+        }
         
         let defaultPlaceholder = UIImage(named: "league_placeholder")
+        let logoURLString = leagueLogos[indexPath.row]
         
         if let url = URL(string: logoURLString), !logoURLString.isEmpty {
             cell.logoImageView.sd_setImage(
                 with: url,
                 placeholderImage: defaultPlaceholder,
-                options: [.retryFailed, .continueInBackground]    )
+                options: [.retryFailed, .continueInBackground]
+            )
         } else {
-               cell.logoImageView.image = defaultPlaceholder
+            cell.logoImageView.image = defaultPlaceholder
         }
         
         return cell
     }
     
-    // الأنميشن مكانه الصح هنا عشان الـ Cell تكون ظهرت بالـ Frames المظبوطة
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if let leagueCell = cell as? LeagueCell {
             leagueCell.startFlashying()
